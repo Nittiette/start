228a45c34e3a6d963da59c7aa1a167a8ee28fb0c
Nittiette <bkryczka7@gmail.com>
2023-11-16 17:55:30 +0100

    first commit

---
+++ start.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float wspolczynnik = 0.7355;
+    
+    for(int km = 10; km <= 100; km += 10)
+    {
+        printf("k m ");
+    }
+    
+    return 0;
+}
